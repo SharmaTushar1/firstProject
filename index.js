29a57ca2856d0e81a4ce2aa4b9385fa96441a7af
suspiciouscouch <sungjinlim.real@gmail.com>
2021-12-16 21:00:13 +0700

    tried adding button functionality

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 // variables
 const output = document.querySelector(".secondRow");
+const operators = document.querySelectorAll(".operator");
 // buttons
 const buttons = document.getElementsByClassName("button");
-const del = document.querySelector(".del");
-const resetButton = document.querySelector(".reset");
-const minus = document.querySelector(".minus");
-const plus = document.querySelector(".plus");
-const dot = document.querySelector(".dot");
-const divide = document.querySelector(".divide");
-const equal = document.querySelector(".equal");
-const times = document.querySelector(".x");
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
-    output.insertAdjacentHTML("beforeend", buttons[i].innerHTML);
+    let btn = button[i].innerHTML;
+    if (btn !== "DEL" && btn !== "RESET") {
+      output.insertAdjacentHTML("beforeend", buttons[i].innerHTML);
+    }
 
     calculate();
   });
