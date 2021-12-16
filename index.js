7a3b52600356a1a8d16ffed6791130b6536522c1
suspiciouscouch <sungjinlim.real@gmail.com>
2021-12-16 16:34:11 +0700

    added button functionality, output in console

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 // variables
 const output = document.querySelector(".secondRow");
+// buttons
+const buttons = document.getElementsByClassName("button");
+const del = document.querySelector(".del");
 const resetButton = document.querySelector(".reset");
+const minus = document.querySelector(".minus");
+const plus = document.querySelector(".plus");
+const dot = document.querySelector(".dot");
+const divide = document.querySelector(".divide");
+const equal = document.querySelector(".equal");
+const times = document.querySelector(".x");
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    output.insertAdjacentHTML("beforeend", buttons[i].innerHTML);
+
+    calculate();
+  });
+}
+
+function calculate() {
+  console.log(math.evaluate(output.innerHTML));
+}
