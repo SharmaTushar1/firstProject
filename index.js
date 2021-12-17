49808e5db0102bf86f48e3b7f0f2bb1d40624174
suspiciouscouch <sungjinlim.real@gmail.com>
2021-12-17 09:24:18 +0700

    all functionality present (except for x being *)

--- index.js
+++ index.js
@@ -7,16 +7,21 @@ const buttons = document.getElementsByClassName("button");
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
     let btn = buttons[i].innerHTML;
+    if (btn == "RESET") {
+      output.innerHTML = " ";
+    }
     if (btn == "DEL") {
       output.innerHTML = output.innerHTML.slice(0, -1);
-    } else {
+    } else if (btn !== "DEL" && btn !== "RESET" && btn !== "=") {
       output.insertAdjacentHTML("beforeend", buttons[i].innerHTML);
     }
 
-    calculate();
+    if (btn == "=") {
+      calculate();
+    }
   });
 }
 
 function calculate() {
-  console.log(math.evaluate(output.innerHTML));
+  output.innerHTML = math.evaluate(output.innerHTML);
 }
