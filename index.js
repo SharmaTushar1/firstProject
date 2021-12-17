dc68a31c0612f21ca1d323ea4bea4dfade73adc3
suspiciouscouch <sungjinlim.real@gmail.com>
2021-12-17 09:17:49 +0700

    allows for operators function and delete

--- index.js
+++ index.js
@@ -1,32 +1,17 @@
 // variables
 const output = document.querySelector(".secondRow");
-<<<<<<< HEAD
-// buttons
-const buttons = document.getElementsByClassName("button");
-const del = document.querySelector(".del");
-const resetButton = document.querySelector(".reset");
-const minus = document.querySelector(".minus");
-const plus = document.querySelector(".plus");
-const dot = document.querySelector(".dot");
-const divide = document.querySelector(".divide");
-const equal = document.querySelector(".equal");
-const times = document.querySelector(".x");
-
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function () {
-    output.insertAdjacentHTML("beforeend", buttons[i].innerHTML);
-=======
 const operators = document.querySelectorAll(".operator");
 // buttons
 const buttons = document.getElementsByClassName("button");
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
-    let btn = button[i].innerHTML;
-    if (btn !== "DEL" && btn !== "RESET") {
+    let btn = buttons[i].innerHTML;
+    if (btn == "DEL") {
+      output.innerHTML = output.innerHTML.slice(0, -1);
+    } else {
       output.insertAdjacentHTML("beforeend", buttons[i].innerHTML);
     }
->>>>>>> parent of b62ae40 (Update index.js)
 
     calculate();
   });
