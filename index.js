82bd456c99e7c315d45ad81ccd46916ac4eac720
suspiciouscouch <sungjinlim.real@gmail.com>
2021-12-17 15:31:44 +0700

    finished themes

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ const output = document.querySelector(".secondRow");
 const operators = document.querySelectorAll(".operator");
 // buttons
 const buttons = document.getElementsByClassName("button");
+const themeButtons = document.querySelectorAll("input");
 
+// theme buttons
+
+for (let i = 0; i < themeButtons.length; i++) {
+  themeButtons[i].addEventListener("click", function () {
+    let tb = themeButtons[i];
+    if (tb.id == "first") {
+      document.getElementById("switcher-id").href = " ";
+    } else if (tb.id == "second") {
+      document.getElementById("switcher-id").href = "./css/light.css";
+    } else if (tb.id == "third") {
+      document.getElementById("switcher-id").href = "./css/dark.css";
+    }
+  });
+}
+// calc buttons
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
     let btn = buttons[i].innerHTML;
